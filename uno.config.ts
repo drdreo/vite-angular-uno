0dc9731a438d0dfbb94d7e5002becbd14fa55394
DrDreo <code@anderas-hahn.at>
2024-05-11 12:22:38 +0200

    configure Uno CLI and angular

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,20 @@ import { defineConfig } from 'unocss'
 
 export default defineConfig({
     rules: [
-        ['m-1', { margin: '1px' }],
+        ['h-1', { 'font-size': '20px', color: '#daa420' }],
+        [/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })],
     ],
+    cli: {
+        entry: {
+            /**
+             * Glob patterns to match files
+             * Include HTML and inline templates in components.
+             */
+            patterns: ["src/**/*.html", "src/**/*.ts"],
+            /**
+             * The output filename for the generated UnoCSS file
+             */
+            outFile: "./src/uno.css"
+        },
+    },
 })
