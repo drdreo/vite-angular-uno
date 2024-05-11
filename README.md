73f5a47d658f2f20ab1fc6993acbd9bd7f95c9f8
DrDreo <code@anderas-hahn.at>
2024-05-11 13:19:47 +0200

    Write documentation

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.7.
 
-## Setup
-via CLI
+No config eject required to use UnoCSS with Angular. Simply install the package and utilize the CLI.
 
-2. `npm i -D unocss`
-3. `npx unocss init`
+## Setup
+1. `npm i`
+2. Run `npm run dev-css`
+3. Run `npm run start`
+
+### JetBrains
+Use community plugin for intellisense - https://unocss.dev/integrations/jetbrains
+
+### VSCode
+For the charlies, use the `unocss` extension - https://unocss.dev/integrations/vscode
 
 ## TODO
-ESLint setup with uno
+- ESLint setup with uno
+- check out layers
+- checkout presets
--- uno.config.ts
+++ uno.config.ts
@@ -1,10 +1,18 @@
-
-import { defineConfig } from 'unocss'
+import {defineConfig, presetUno, presetAttributify, presetWebFonts} from 'unocss'
 
 export default defineConfig({
+    presets: [
+        presetUno(),
+        presetWebFonts({
+            provider: 'google',
+            fonts: {
+                sans: 'Montserrat',
+                mono: ['Fira Code', 'Fira Mono:400,700'],
+            },
+        })], // presets: [] -  disable default presets - includes preflight layer
     rules: [
-        ['h-1', { 'font-size': '20px', color: '#daa420' }],
-        [/^m-([\.\d]+)$/, ([_, num]) => ({ margin: `${num}px` })],
+        ['h-1', {'font-size': '20px', color: '#daa420'}],
+        [/^m-([\.\d]+)$/, ([_, num]) => ({margin: `${num}px`})],
     ],
     cli: {
         entry: {
